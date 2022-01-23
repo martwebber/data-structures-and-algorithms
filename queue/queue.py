407e83b34011d2ee90042a20793eb35492ff4fc1
martwebber <49777774+martwebber@users.noreply.github.com>
2022-01-23 23:03:17 +0300

    Enqueue and dequeue operations

---
+++ queue/queue.py
@@ -0,0 +1,33 @@
+class QueueDS:
+    def __init__(self):
+        self.queue = []
+    def enqueue(self):
+        val = input("Enter an element: ")
+        self.queue.append(val)
+        print(f'{val} has been added to the queue')
+
+    def dequeue(self):
+        if not self.queue:
+            print("The queue is empty")
+        else:
+            elem = self.queue.pop(0)
+            print(f"{elem} has been removed")
+
+    def display(self):
+        print(self.queue)
+
+if __name__ == '__main__':
+    q1 = QueueDS()
+    while True:
+        print("Select 1 to add an element, 2 to remove an element, 3 to display and 4 to quit")
+        choice = int(input("What is your choice?"))
+        if choice == 1:
+            q1.enqueue()
+        elif choice == 2:
+            q1.dequeue()
+        elif choice == 3:
+            q1.display()
+        elif choice == 4:
+            break
+        else:
+            print("You entered a wrong choice, try again")
