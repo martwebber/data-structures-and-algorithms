fdadaab1b2cf0eedb90007f9b7197c6ba5bfafe0
martwebber <49777774+martwebber@users.noreply.github.com>
2022-01-26 23:08:26 +0300

    binary search tree - delete and traverse operations

--- tree/binary-search-tree.py
+++ tree/binary-search-tree.py
@@ -43,11 +43,69 @@ class BinarySearchTree:
             else:
                 print("Node not found")
     def deletion(self,data):
-        pass
+        if self.key is None:
+            print("The tree is empty")
+            return
+        if data < self.key:
+            if self.left_child:
+                self.left_child = self.left_child.deletion(data)
+            else:
+                print("The node is not present")
+        elif data > self.key:
+            if self.right_child:
+                self.right_child = self.right_child.deletion(data)
+            else:
+                print("The node is not present")
+        else:
+            if self.left_child is None:
+                temp =self.right_child
+                self = None
+                return temp
+            if self.right_child is None:
+                temp =self.left_child
+                self = None
+                return temp
+            node = self.right_child
+            while  node.left_child:
+                node = node.left_child
+            self.key = node.key
+            self.right_child = self.right_child.deletion(node.key)
+        return self
 
+
+
+
+    def preorder_traversal(self):
+        print(self.key, end=' ')
+        if self.left_child:
+            self.left_child.preorder_traversal()
+            if self.right_child:
+                self.right_child.preorder_traversal()
+    def inorder_traversal(self):
+        if self.left_child:
+            self.left_child.inorder_traversal()
+        print(self.key, end=' ')
+        if self.right_child:
+            self.right_child.inorder_traversal()
+    def postorder_traversal(self):
+        if self.left_child:
+            self.left_child.postorder_traversal()
+        if self.right_child:
+            self.right_child.postorder_traversal()
+        print(self.key, end=' ')
 if __name__ == '__main__':
-    root = BinarySearchTree(None)
+    root = BinarySearchTree(50)
     l1 = [90,76,90,4,74,23,0,12]
     for i in l1:
         root.insertion(i)
     root.search(40)
+    print("Pre-order")
+    root.preorder_traversal()
+    print("\nin-order")
+    root.inorder_traversal()
+    print("\npost-order")
+    root.postorder_traversal()
+    root.deletion(50)
+    print("\npost-order after deleting node")
+    root.postorder_traversal()
+
